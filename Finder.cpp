caef6be08488ef085c709e683a659830ac23938e
ShashwatSingh67 <a1970638@adelaide.edu.au>
2026-03-02 12:57:04 +1030

    sigma

--- Finder.cpp
+++ Finder.cpp
@@ -1,14 +1,18 @@
 #include "Finder.h"
 using namespace std;
-vector<int> Finder::findSubstrings(string s1, string s2) {
 
-    vector<int> result;
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    int prev = 0;
+    vector<int> result(s1.size(), -1);
     for(size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i));
+
+        size_t found = s1.find(s2.substr(0, i), prev);
+
         if (found != string::npos) {
             result.push_back(found);
+            prev = i;
         } else {
-            result.push_back(-1);
+            break;
         }
     }
     return result;
